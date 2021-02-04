8b8b6fd6f675c14ae9a72f8cbe853580e7ffd024
thibautloiseau <thibaut.loiseau@gmail.com>
2021-02-04 19:44:17 +0100

    MUSIC sur tous les paquets

--- main.py
+++ main.py
@@ -3,38 +3,44 @@ import process_csi as process
 import matplotlib.pyplot as plt
 
 def main():
-    measures = []
-    expMeasures = []
-
     for doc in os.listdir("setup/1tx_3rx"):
         print(doc)
-        expMeasures.append(float(doc))
         CSI = process.CSI("setup/1tx_3rx/" + doc)
-        measures.append(CSI.pseudo_spectrum()[-1])
+        CSI.pseudo_spectrum()
 
-    plt.figure()
-    plt.title("Measures for calibration")
-    plt.xlim(-90, 90)
-    plt.ylim(-90, 90)
-    plt.xlabel("Effective measures with MUSIC algorithm (in degrees)")
-    plt.ylabel("Expected measures (in degrees)")
-    plt.plot(measures, expMeasures, '+')
-    plt.show()
 
-    for doc in os.listdir("Philibert/"):
-        print(doc)
-        expMeasures.append(float(doc))
-        CSI = process.CSI("Philibert/" + doc)
-        measures.append(CSI.pseudo_spectrum()[-1])
-
-    plt.figure()
-    plt.title("Measures for calibration")
-    plt.xlim(-90, 90)
-    plt.ylim(0, 90)
-    plt.xlabel("Effective measures with MUSIC algorithm (in degrees)")
-    plt.ylabel("Expected measures (in degrees)")
-    plt.plot(measures, expMeasures, '+')
-    plt.show()
+    # measures = []
+    # expMeasures = []
+    #
+    # for doc in os.listdir("setup/1tx_3rx"):
+    #     print(doc)
+    #     expMeasures.append(float(doc))
+    #     CSI = process.CSI("setup/1tx_3rx/" + doc)
+    #     measures.append(CSI.pseudo_spectrum()[-1])
+    #
+    # plt.figure()
+    # plt.title("Measures for calibration")
+    # plt.xlim(-90, 90)
+    # plt.ylim(-90, 90)
+    # plt.xlabel("Effective measures with MUSIC algorithm (in degrees)")
+    # plt.ylabel("Expected measures (in degrees)")
+    # plt.plot(measures, expMeasures, '+')
+    # plt.show()
+    #
+    # for doc in os.listdir("Philibert/"):
+    #     print(doc)
+    #     expMeasures.append(float(doc))
+    #     CSI = process.CSI("Philibert/" + doc)
+    #     measures.append(CSI.pseudo_spectrum()[-1])
+    #
+    # plt.figure()
+    # plt.title("Measures for calibration")
+    # plt.xlim(-90, 90)
+    # plt.ylim(0, 90)
+    # plt.xlabel("Effective measures with MUSIC algorithm (in degrees)")
+    # plt.ylabel("Expected measures (in degrees)")
+    # plt.plot(measures, expMeasures, '+')
+    # plt.show()
 
 
 if __name__ == "__main__":
--- process_csi.py
+++ process_csi.py
@@ -156,12 +156,17 @@ class CSI:
                 amp_means[i, j] = [np.mean(res[:, i, j, k]) for k in range(res.shape[3])]
                 amp_std[i, j] = [np.std(res[:, i, j, k]) for k in range(res.shape[3])]
 
+                # On garde les valeurs brutes pour les premières valeurs et les dernières qui ne sont pas moyennées avec les deux voisines
+                amp_filters[0, i, j, :] = res[0, i, j, :]
+                amp_filters[-1, i, j, :] = res[-1, i, j, :]
+
+                # On traite toutes les autres valeurs en moyennant sur les deux voisines
                 for k in range(1, res.shape[0] - 1):
                     amp_filter = 1 / 3 * (res[k - 1, i, j, :] + res[k, i, j, :] + res[k + 1, i, j, :])
                     if np.cov(amp_filter, amp_means[i, j])[0, 1] > 0:
                         amp_filters[k, i, j, :] = amp_filter
 
-        return amp_filters[1: -1]
+        return amp_filters
 
     def plot_processed_amp(self):
         """Tracer l'amplitude corrigée"""
@@ -191,8 +196,9 @@ class CSI:
                 for j in range(res.shape[2]):
                     slope = (res[k, i, j, -1] - res[k, i, j, 0]) / (res.shape[3] - 1)
                     intercept = np.mean(res[k, i, j, :])
-                    corrected_phases[k, i, j] = res[k, i, j] - slope * np.array(
-                        [i for i in range(res.shape[3])]) - intercept
+                    corrected_phases[k, i, j] = res[k, i, j] - slope * np.array([i for i in range(res.shape[3])]) - intercept
+
+        corrected_phases = corrected_phases - corrected_phases[0, 0, 0, 0]
 
         return corrected_phases
 
@@ -263,34 +269,46 @@ class CSI:
         amps = self.process_amp()
         phases = self.process_phase()
 
-        mean_amps = amps.mean(axis=0)
-        mean_phases = phases.mean(axis=0)
+        proc_csi = amps * np.exp(1j * phases)
+        noise_subspaces = np.zeros(shape=(proc_csi.shape[0], self.params['Nrx'], 2), dtype=complex)
 
-        mean_csi = mean_amps * np.exp(1j * mean_phases)
-        mean_csi = np.reshape(mean_csi, (mean_csi.shape[0] * mean_csi.shape[1], mean_csi.shape[2]))
+        for i in range(proc_csi.shape[0]):
+            paquet = proc_csi[i]
+            paquet = np.reshape(paquet, (paquet.shape[0] * paquet.shape[1], paquet.shape[2]))
 
-        Rx = np.dot(mean_csi, np.conj(mean_csi.T))
+            Rx = np.dot(paquet, np.conj(paquet.T))
 
-        eigvals, eigvecs = np.linalg.eig(Rx)
+            eigvals, eigvecs = np.linalg.eig(Rx)
 
-        # On ordonne les valeurs propres et les vecteurs propres par ordre décroissant
-        idx = eigvals.argsort()[::-1]
-        eigvecs = eigvecs[:, idx]
+            # On ordonne les val p et les vec p par ordre décroissant pour récupérer le sous espace bruit constitué des deux vec p issues des deux val p les plus petites
+            idx = eigvals.argsort()[::-1]
+            eigvecs = eigvecs[:, idx]
 
-        noise_subspace = eigvecs[:, -2:]
+            noise_subspace = eigvecs[:, -2:]
+            noise_subspaces[i] = noise_subspace
 
-        return noise_subspace
+        return noise_subspaces
 
     def pseudo_spectrum(self):
-        """Récupérer le pseudo-spectre issu de l'algorithme MUSIC"""
-        noise_subspace = self.noise_subspace()
+        """Récupérer les pseudo-spectre issu de l'algorithme MUSIC pour chaque paquet"""
+        noise_subspaces = self.noise_subspace()
 
         omegas = np.linspace(-np.pi, np.pi, 360)
-        e_omegas = np.array([np.exp(1j * i * omegas) for i in range(noise_subspace.shape[0])]).T
+        e_omegas = np.array([np.exp(1j * i * omegas) for i in range(noise_subspaces.shape[1])]).T
 
-        inv_spectrum = np.array([np.dot(np.dot(np.dot(np.conj(e_omegas[i]), noise_subspace), np.conj(noise_subspace).T), e_omegas[i]) for i in range(e_omegas.shape[0])])
+        pseudo_spectrums = np.zeros(shape=(noise_subspaces.shape[0], e_omegas.shape[0]), dtype=complex)
+        maximums = np.zeros(shape=(noise_subspaces.shape[0]))
 
-        return (omegas, np.abs(1 / inv_spectrum), 180 / np.pi * omegas[np.argmax(1 / inv_spectrum)])
+        for i in range(noise_subspaces.shape[0]):
+            inv_spectrum = np.array([np.dot(np.dot(np.dot(np.conj(e_omegas[j]), noise_subspaces[i]), np.conj(noise_subspaces[i]).T), e_omegas[j]) for j in range(e_omegas.shape[0])])
+
+            pseudo_spectrums[i] = np.reciprocal(np.abs(inv_spectrum))
+            maximums[i] = 180 / np.pi * omegas[np.argmax(pseudo_spectrums[i])]
+
+        print(np.mean(maximums))
+        print(np.std(maximums))
+
+        return (omegas, pseudo_spectrums, maximums)
 
     def plot_pseudo_spectrum(self):
         """Tracé du pseudo-spectre de l'algorithme MUSIC"""
